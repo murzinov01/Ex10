bd24bba3bb69e28a8eb64e572f65d6f18b0a0ce1
Michail <mdmurzinov@edu.hse.ru>
2021-01-19 23:04:24 +0300

    hot fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
   std::cout << "Enter the string: ";
   std::getline(std::cin, str);
   std::cout << "Initial string: " << str << std::endl;
-  std::string postfix_str = infix2prefix(str);
+  std::string postfix_str = infix2postfix(str);
   std::cout << "Result: " << postfix_str << std::endl;
   return 0;
 }
